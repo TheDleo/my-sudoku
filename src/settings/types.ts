878873369f5c772dca9b0343cf8e10dc2108fe2b
Dan Martin <dan@dleo.info>
2026-05-30 14:27:33 -0700

    feat(settings): add Settings and Theme types

---
+++ src/settings/types.ts
@@ -0,0 +1,9 @@
+export type Theme = 'auto' | 'light' | 'dark';
+
+export type Settings = {
+  autoCandidates: boolean;
+  possiblePlacements: boolean;
+  showTimer: boolean;
+  showMistakes: boolean;
+  theme: Theme;
+};
